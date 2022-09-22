2bc0ca2e5f005babc5f4c6f65adc7e7e41fd75d1
fatihatac <fatac2000@gmail.com>
2022-09-22 20:03:58 +0300

    Validation Rules added

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(c => c.Email).NotEmpty();
+            RuleFor(c=>c.FirstName).NotEmpty();
+            RuleFor(c=>c.LastName).NotEmpty();
+            RuleFor(c=>c.Password).NotEmpty();
+            RuleFor(c => c.Password).MinimumLength(4);
+            //RuleFor(c => c.Password).Must(PasswordContent);
+        }
+    }
+}
